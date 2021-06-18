8736318a4de7797d66fa1d98a7fa954ff93e7b01
Azgaar <maxganiev@yandex.ru>
2021-06-18 14:17:09 +0300

    add typescript and linting

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  parser: 'typescript',
+};
