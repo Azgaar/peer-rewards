1cd193449718d41a6152389d1f0fdc633fb292da
Azgaar <maxganiev@yandex.ru>
2021-06-19 15:13:44 +0300

    reward to be number

--- src/pages/GivenRewards.tsx
+++ src/pages/GivenRewards.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import UserAvatar from '../components/shared/UserAvatar';
+import useStyles from './List.style';
 import { IReward } from '../types';
+import { getFullName, getRelativeDate } from '../utils';
 
 const GivenRewards = ({ rewards }: { rewards: IReward[] }): JSX.Element => {
-  return <Container maxWidth="md">GivenRewards</Container>;
+  const classes = useStyles();
+
+  return (
+    <List className={classes.list}>
+      {rewards.map(({ id, to, reward, datetime, message }) => {
+        const rewardee = getFullName(to);
+        const date = getRelativeDate(datetime);
+
+        return (
+          <ListItem key={id}>
+            <ListItemAvatar>
+              <UserAvatar user={to} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={message}
+              secondary={
+                <span>
+                  {`You rewarded ${rewardee} `}
+                  <b>{reward}</b>
+                  {` ${date}`}
+                </span>
+              }
+            />
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 };
 
 export default GivenRewards;
